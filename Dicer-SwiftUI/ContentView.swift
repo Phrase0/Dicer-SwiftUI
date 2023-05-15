c75420896ae1e4a034be377371e9bf99f7a7348b
Peiyun <b612starnight@gmail.com>
2023-05-15 23:02:17 +0800

    Initial Commit

---
+++ Dicer-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicer-SwiftUI
+//
+//  Created by Peiyun on 2023/5/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dicer-SwiftUI/Dicer_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicer_SwiftUIApp.swift
+//  Dicer-SwiftUI
+//
+//  Created by Peiyun on 2023/5/15.
+//
+
+import SwiftUI
+
+@main
+struct Dicer_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
